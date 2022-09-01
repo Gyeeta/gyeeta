a39bc3c90f9ba9bae314bfc468b26b87c8188589
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-09-01 19:11:10 +0530

    Change for testing partha
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- partha/test_multi_partha.sh
+++ partha/test_multi_partha.sh
@@ -36,12 +36,12 @@ for i in `seq $STARTID $(( $STARTID + $NUMPARTHA ))`;do
 
 	mkdir -p $PARDIR/{cfg,log,tmp} 2> /dev/null
 
+	cp -a ../install/partha/* $PARDIR 2> /dev/null
+
 	\rm -f $PARDIR/log/*.log 2> /dev/null
 	 
 	ln -s /dev/null $PARDIR/log/partha.log
 	ln -s /dev/null $PARDIR/log/parmon.log
-
-	cp -a `pwd`/{lib,partha,runpartha.sh} $PARDIR 2> /dev/null
 
 	sudo -n setcap cap_chown,cap_dac_override,cap_dac_read_search,cap_fowner,cap_fsetid,cap_ipc_lock,cap_kill,cap_mac_admin,cap_mknod,cap_sys_chroot,cap_sys_resource,cap_setpcap,cap_sys_ptrace,cap_sys_admin,cap_net_admin,cap_net_raw,cap_sys_module+ep $PARDIR/partha || echo
 
