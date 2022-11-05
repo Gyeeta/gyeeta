0bd95d6b50fd930d21b19a0723b2e61065034303
Krishna <35102612+kvdevel@users.noreply.github.com>
2022-11-05 21:12:54 +0530

    Added deploy script
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

---
+++ deployinstall.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo -e "\n\nUsage : $0 <Install Base Dir>\n\n"
+	exit 1
+fi	
+
+set -x 
+
+ODIR="$1"
+ODIR_SHYAMA="$ODIR"/shyama
+ODIR_MADHAVA="$ODIR"/madhava
+ODIR_PARTHA="$ODIR"/partha
+
+mkdir -p $ODIR_SHYAMA $ODIR_MADHAVA $ODIR_PARTHA 2> /dev/null
+
+if [ ! -d $ODIR_SHYAMA ] || [ ! -d $ODIR_MADHAVA ] || [ ! -d $ODIR_PARTHA ]; then
+	echo -e "\n\nERROR : Failed to create install Gyeeta install dirs\n\n"
+	exit 1
+fi	
+
+rm -rf $ODIR_SHYAMA $ODIR_MADHAVA $ODIR_PARTHA 2> /dev/null	
+	
+cp -a ./install/{shyama,madhava,partha} $ODIR/
+
+if [ $? -ne 0 ]; then
+	echo -e "\n\nERROR : Failed to copy to Gyeeta install dirs\n\n"
+	exit 1
+fi
+
+sudo -n setcap cap_chown,cap_dac_override,cap_dac_read_search,cap_fowner,cap_fsetid,cap_ipc_lock,cap_kill,cap_mac_admin,cap_mknod,cap_sys_chroot,cap_sys_resource,cap_setpcap,cap_sys_ptrace,cap_sys_admin,cap_net_admin,cap_net_raw,cap_sys_module+ep $ODIR_PARTHA/partha
+
+if [ $? -ne 0 ]; then
+	echo -e "\n\nERROR : Failed to setcap partha binary\n\n"
+	exit 1
+fi
+
+echo -e "\nInstalled shyama, madhava and partha to $ODIR successfully...\n"
+
+exit 0
