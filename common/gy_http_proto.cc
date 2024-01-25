29934b2326768b5be302ef6123415c3e8ffd9d4f
Krishna <35102612+kvdevel@users.noreply.github.com>
2024-01-25 19:37:35 +0530

    Adding HTTP request parsing wip
    Signed-off-by: Krishna <35102612+kvdevel@users.noreply.github.com>

--- common/gy_http_proto.cc
+++ common/gy_http_proto.cc
@@ -51,40 +51,191 @@ void HTTP1_PROTO::print_stats(STR_WR_BUF & strbuf, time_t tcur, time_t tlast) no
 
 HTTP1_SESSINFO::HTTP1_SESSINFO(HTTP1_PROTO & prot, SVC_SESSION & svcsess, PARSE_PKT_HDR & hdr)
 	: HTTP1_PROTO(prot), 
+	tran_(svcsess.common_.tlastpkt_usec_, svcsess.common_.tconnect_usec_, svcsess.common_.cli_ipport_, svcsess.common_.ser_ipport_, svcsess.common_.glob_id_, svcsess.proto_), 
+	tdstrbuf_(prot.api_max_len_ - 1), 
 	svcsess_(svcsess), is_https_(hdr.src_ == SRC_UPROBE_SSL)
 {
+	std::memset(tdstrbuf_.get(), 0, 128);
+	
+	gstats[STATH1_NEW_SESS]++;
+	
+	if (svcsess.common_.syn_seen_ == false) {
+		is_midway_session_ = 1;
+		
+		// TODO
 
+		gstats[STATH1_MIDWAY_SESS]++;
+	}
 }
 
 HTTP1_SESSINFO::~HTTP1_SESSINFO() noexcept
 {
+	gstats[STATH1_SESS_COMPLETE]++;
 
+	if (part_query_started_ == 1) {
+		drop_partial_req();
+	}
 }
 
 int HTTP1_SESSINFO::handle_request_pkt(PARSE_PKT_HDR & hdr, uint8_t *pdata)
 {
+	if (skip_session_ == 1) {
+		gstats[STATH1_SESS_SKIP_PKT]++;
+		return 0;
+	}
+
+	if (drop_seen_ || common.clidroptype_ == DT_DROP_SEEN || common.serdroptype_ == DT_DROP_SEEN) {
+
+		if (part_query_started_ == 1) {
+			drop_partial_req();
+			part_query_started_ = 0;
+		}
+			
+		// TODO
+		drop_seen_ = 0;
+
+		return 0;
+	}
+
+	ret = parse_req_pkt(hdr, pdata);
+	if (ret < 0) {
+		gstats[STATH1_REQ_PKT_SKIP]++;
+		return 0;
+	}	
+
+	tran_.update_req_stats(common.tlastpkt_usec_, hdr.datalen_);
+
+	if (tdstrbuf_.size() && part_query_started_ == 0) {
+		tran_.request_len_ = tdstrbuf_.size() + 1;
+		part_query_started_ = 1;
+		set_partial_req();
+	}
+
 	return 0;
 }
 
 int HTTP1_SESSINFO::handle_response_pkt(PARSE_PKT_HDR & hdr, uint8_t *pdata)
 {
+	uint8_t 		*sptr = pdata;
+	uint32_t 		bytes_left, pktlen = hdr.datalen_;
+	auto			& common = svcsess_.common_;
+	int 			ret;
+
+	if (skip_session_ == 1) {
+		gstats[STATH1_SESS_SKIP_PKT]++;
+		return 0;
+	}
+
+	if (drop_seen_ || common.clidroptype_ == DT_DROP_SEEN || common.serdroptype_ == DT_DROP_SEEN) {
+
+drop_chk :
+		if (part_query_started_ == 1) {
+			drop_partial_req();
+			part_query_started_ = 0;
+		}
+			
+		drop_seen_ = 0;
+
+		// TODO
+
+		return 0;
+	}
+
+	tran_.update_resp_stats(common.tlastpkt_usec_, hdr.datalen_);
+
+	ret = parse_resp_pkt(hdr, pdata);
+	if (ret < 0) {
+		gstats[STATH1_RESP_PKT_SKIP]++;
+	}	
+
 	return 0;
 }
 
 void HTTP1_SESSINFO::handle_session_end(PARSE_PKT_HDR & hdr)
 {
+	auto				& common = svcsess_.common_;
+
+	if (part_query_started_ == 1) {
+		drop_partial_req();
+		part_query_started_ = 0;
+	}
+	
+	if (tdstat_.reqnum_ == 0) {
+		return;
+	}	
+
+	/*
+	 * Flush existing request
+	 */
+	if (tran_.reqlen_ && tdstrbuf_.size() > 0 && (!(drop_seen_ || common.clidroptype_ == DT_DROP_SEEN || common.serdroptype_ == DT_DROP_SEEN))) {
+		if (tran_.reslen_ == 0) {
+			tran_.tupd_usec_ = common.tlastpkt_usec_;
+			tran_.tres_usec_ = common.tlastpkt_usec_;
+
+			tran_.reslen_ = 1;
+		}	
+
+		request_done();
+	}	
+	else {
+		request_done(false /* flushreq */, true /* clear_req */);
+	}	
 
 }
 
 void HTTP1_SESSINFO::handle_ssl_change(PARSE_PKT_HDR & hdr, uint8_t *pdata)
 {
-
+	is_https_ = true;
 }	
 
 void HTTP1_SESSINFO::set_new_req() noexcept
 {
+	COMMON_PROTO 				& common = svcsess_.common_;
 
+	tran_.reset();
+
+	tran_.treq_usec_			= common.tlastpkt_usec_;
+	tran_.tupd_usec_			= common.tlastpkt_usec_;
+	tran_.tin_usec_				= common.tlastpkt_usec_;
+	
+	reset_on_new_req();
 }
+
+void HTTP1_SESSINFO::reset_on_new_req() noexcept
+{
+	tdstrbuf_.reset();
+	reqbody_buf_.reset();
+	errorbuf_.reset();
+}
+
+void HTTP1_SESSINFO::reset_req_frag_stat() noexcept
+{
+	req_tkn_frag_ = 0;
+	memset(reqhdr_, 0, sizeof(reqhdr_));
+	req_hdr_fraglen_ = 0;
+	nbytes_req_frag_ = 0;
+	skip_req_bytes_ = 0;
+}	
+
+void HTTP1_SESSINFO::reset_resp_frag_stat() noexcept
+{
+	resp_tkn_frag_ = 0;
+	memset(resphdr_, 0, sizeof(resphdr_));
+	resp_hdr_fraglen_ = 0;
+	nbytes_resp_frag_ = 0;
+	skip_resp_bytes_ = 0;
+}	
+
+void HTTP1_SESSINFO::reset_stats_on_resp(bool clear_req) noexcept
+{
+	if (clear_req) {	
+		reset_req_frag_stat();	
+	}
+
+	reset_resp_frag_stat();
+	
+	// TODO
+}	
 
 int HTTP1_SESSINFO::parse_req_pkt(PARSE_PKT_HDR & hdr, uint8_t *pdata)
 {
@@ -97,6 +248,21 @@ int HTTP1_SESSINFO::parse_resp_pkt(PARSE_PKT_HDR & hdr, uint8_t *pdata)
 }
 
 void HTTP1_SESSINFO::request_done(bool flushreq, bool clear_req)
+{
+
+}
+
+void HTTP1_SESSINFO::reset_on_error()
+{
+
+}
+
+bool HTTP1_SESSINFO::print_req() noexcept
+{
+	return true;
+}
+
+void HTTP1_SESSINFO::print_stats(STR_WR_BUF & strbuf, time_t tcur, time_t tlast) noexcept
 {
 
 }
--- common/gy_http_proto_detail.h
+++ common/gy_http_proto_detail.h
@@ -4,75 +4,117 @@
 #pragma				once
 
 #include			"gy_http_proto.h"
+#include			"gy_proto_parser.h"
 
 namespace gyeeta {
 
-struct HTTP_REQSTAT
+enum REQ_HDR_HTTP : uint8_t
 {
-	uint8_t				req_tkn_frag_			{0};	
-	uint8_t				resp_tkn_frag_			{0};	
-	uint8_t				reqhdr_[8]			{};
-	uint8_t				resphdr_[8]			{};
-	uint8_t				req_hdr_fraglen_		{0};
-	uint8_t				resp_hdr_fraglen_		{0};
-	uint32_t			nbytes_req_frag_		{0};
-	uint32_t			nbytes_resp_frag_		{0};
-	uint32_t			skip_req_bytes_			{0};
-	uint32_t			skip_resp_bytes_		{0};
-	uint16_t			nready_resp_pending_		{0};
+	REQ_HDR_CONTENT_TYPE,
+	REQ_HDR_CONTENT_LEN,
+	REQ_HDR_TRANSFER_ENCODE,
+};
 
-	uint8_t				skip_till_auth_resp_		{0};
-	uint8_t				skip_to_req_after_resp_		{0};
-	uint8_t				skip_req_till_sync_		{0};
-	uint8_t				skip_req_resp_till_ready_	{0};
-	uint8_t				drop_seen_			{0};
-	uint8_t				copy_mode_			{0};
+enum RESP_HDR_HTTP : uint8_t
+{
+	RESP_HDR_CONTENT_TYPE,
+	RESP_HDR_CONTENT_LEN,
+	RESP_HDR_CONTENT_ENCODING,
+	RESP_HDR_TRANSFER_ENCODE,
+	RESP_HDR_UPGRADE,
+	RESP_HDR_CONNECTION,
+};
 
-	void reset_req_frag_stat() noexcept
-	{
-		req_tkn_frag_ = 0;
-		memset(reqhdr_, 0, sizeof(reqhdr_));
-		req_hdr_fraglen_ = 0;
-		nbytes_req_frag_ = 0;
-		skip_req_bytes_ = 0;
-	}	
-
-	void reset_resp_frag_stat() noexcept
-	{
-		resp_tkn_frag_ = 0;
-		memset(resphdr_, 0, sizeof(resphdr_));
-		resp_hdr_fraglen_ = 0;
-		nbytes_resp_frag_ = 0;
-		skip_resp_bytes_ = 0;
-	}	
-
-	void reset_stats_on_resp(bool clear_req = false) noexcept
-	{
-		if (clear_req) {	
-			reset();	
-			return;
-		}
-
-		reset_resp_frag_stat();
-		
-		skip_till_auth_resp_ = 0;
-		skip_to_req_after_resp_ = 0;
-		skip_req_till_sync_ = 0;
-		skip_req_resp_till_ready_ = 0;
-		copy_mode_ = 0;
-		nready_resp_pending_ = 0;
-	}	
-
-	void reset() noexcept
-	{
-		std::memset((void *)this, 0, sizeof(*this));
-	}	
+enum HCONN_STATE_E : uint8_t
+{
+	HCONN_IDLE			= 0,
+	HCONN_START_LINE,
+	HCONN_HDR_ACTIVE,
+	HCONN_DATA_ACTIVE,
+	HCONN_CHUNK_HDR,
+	HCONN_CHUNK_DATA,
 };	
 
+enum CONN_STATE_E : uint8_t
+{
+	CONN_IDLE			= 0,
+	CONN_REQ_ACTIVE,
+	CONN_RESP_NEXT,
+	CONN_RESP_ACTIVE,
+};	
+
+struct HCONN_STAT
+{
+	size_t				data_chunk_len_		{0};
+	size_t				data_chunk_left_	{0};
+	uint16_t			fraglen_		{0};
+	uint16_t			resp_status_		{0};
+	HCONN_STATE_E			state_			{HCONN_IDLE};
+	HTTP1_PROTO::METHODS_E		req_method_		{HTTP1_PROTO::METHOD_GET};
+	bool				data_compressed_	{false};	// Content-Encoding compressed or Transfer Encoding compressed
+};	
 
 class HTTP1_SESSINFO : public HTTP1_PROTO
 {
 public :
+	static constexpr size_t		MAX_PIPELINE_ELEMS	{128};
+	static constexpr size_t		MAX_FRAG_LEN		{512};
+
+	enum STATS_H1_E : uint8_t
+	{
+		STATH1_NEW_SESS		= 0,
+		STATH1_SESS_COMPLETE,
+		STATH1_MIDWAY_SESS,
+		STATH1_SKIP_SESS,
+
+		STATH1_REQ_RESET_ERR,
+		STATH1_RESP_RESET_ERR,
+		STATH1_SESS_SKIP_PKT,
+		STATH1_REQ_PKT_SKIP,
+		STATH1_RESP_PKT_SKIP,
+		STATH1_REQ_PIPELINED,
+		STATH1_REQ_PIPELINE_OVF,
+
+		STATH1_MAX,
+	};	
+
+	static inline constexpr const char *	gstatstr[STATH1_MAX] =
+	{
+		"New Session", "Session Completed", "Midway Session", "Session Skipped", 
+		"Req Reset on Error", "Resp Reset on Error", 
+		"Session Skipped Pkt", "Request Packets Skipped", "Response Packets Skipped",
+		"Request Pipelined", "Req Pipeline Overflow", 
+	};	
+
+	static inline uint64_t		gstats[STATH1_MAX]		{};
+	static inline uint64_t		gstats_old[STATH1_MAX]		{};
+	static inline uint64_t		gtotal_queries = 0, glast_queries = 0, gtotal_resp = 0, glast_resp = 0;
+
+
+	API_TRAN			tran_;
+	STRING_HEAP			tdstrbuf_;
+	STRING_BUFFER<1024>		reqbody_buf_;
+	STRING_BUFFER<256>		errorbuf_;
+	
+	METHODS_E			req_methods_[MAX_PIPELINE_ELEMS]	{};
+	uint16_t			npending_				{0};
+
+	uint8_t				reqfragbuf_[MAX_FRAG_LEN];
+	uint8_t				respfragbuf_[MAX_FRAG_LEN];
+
+	HCONN_STAT			reqstate_;
+	HCONN_STAT			respstate_;
+
+	SVC_SESSION 			& svcsess_;
+
+	bool				is_https_		{false};
+	bool				is_keep_alive_		{false};
+	bool				is_http10_		{false};
+	uint8_t				skip_session_		{false};
+	uint8_t				drop_seen_		{0};
+	uint8_t				part_query_started_	{0};
+	uint8_t				is_midway_session_	{0};
+
 	HTTP1_SESSINFO(HTTP1_PROTO & prot, SVC_SESSION & svcsess, PARSE_PKT_HDR & hdr);
 
 	~HTTP1_SESSINFO() noexcept;
@@ -93,6 +135,14 @@ public :
 
 	void request_done(bool flushreq = true, bool clear_req = false);
 	
+	void reset_on_new_req() noexcept;
+
+	void reset_req_frag_stat() noexcept;
+
+	void reset_resp_frag_stat() noexcept;
+
+	void reset_stats_on_resp(bool clear_req = false) noexcept;
+
 	void reset_on_error();
 	
 	bool print_req() noexcept;
